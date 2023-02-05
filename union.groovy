61d713655ffa158b1a6eb25cc9627d7804e1b4db
JansenSmith <jansen@bstarengineering.com>
2023-02-05 13:17:11 -0500

    fixed typo

--- union.groovy
+++ union.groovy
@@ -8,7 +8,7 @@ CSG sphere = new Sphere(size/20*12.5).toCSG()
 // perform a union
 CSG cubePlusSphere = cube.union(sphere);
 
-//To union a list of CSG's together use the static unioAll
+//To union a list of CSG's together use the static unionAll
 CSG allUnion = CSG.unionAll([cubePlusSphere , cube.movex(size*1.5), sphere.movey(size*1.5)])
 
 return allUnion
